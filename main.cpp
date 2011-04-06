35a8a06cc8d928c26318ae79f314b91f1790d8d4
Niklas Wulf <niklas@wulf-home.de>
2011-04-06 09:40:29 +0200

    Testcode zu main() hinzugefügt.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
+#include <Database/database.h>
+#include <Database/drinks.h>
+#include <Database/drink.h>
+
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    Database::Database *database = new Database::Database();
+    QFile file("/Users/niklas/Dropbox/Public/ProjectStats/projectstats.db");
+    qDebug() << file.exists();
+    database->initialize(file);
+
+    foreach( Database::Drink *d, Database::Drinks::instance()->allRows())
+    {
+        qDebug() << d->id();
+    }
 
     return a.exec();
 }
