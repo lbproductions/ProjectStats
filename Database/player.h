e690381c73681a27df3f1c6c167d3d1f3bcc7389
Niclas Raabe <screamer2002@gmx.de>
2011-04-11 16:04:38 +0200

    Implementierte Player-Klasse
    Signed-off-by: Niclas Raabe <screamer2002@gmx.de>

---
+++ Database/player.h
@@ -0,0 +1,27 @@
+#ifndef DATABASE_PLAYER_H
+#define DATABASE_PLAYER_H
+
+#include "row.h"
+#include "table.h"
+#include "attribute.h"
+#include "databaseattribute.h"
+
+START_ROW_DECLARATION(Player, Row)
+    DECLARE_ROW_CONSTRUCTORS(Player, Player)
+
+    DECLARE_DATABASEATTRIBUTE(QString,Player,name)
+    DECLARE_DATABASEATTRIBUTE(QString,Player,gender)
+    DECLARE_DATABASEATTRIBUTE(int,Player,weight)
+    DECLARE_DATABASEATTRIBUTE(int,Player,size)
+    DECLARE_DATABASEATTRIBUTE(QColor,Player,color)
+    DECLARE_DATABASEATTRIBUTE(QPixmap,Player,avatar)
+    DECLARE_DATABASEATTRIBUTE(int,Player,residence)
+
+    DECLARE_ATTRIBUTE(QList<Player*>, Player, players)
+
+END_ROW_DECLARATION(Player)
+
+START_TABLE_DECLARATION(Player)
+END_TABLE_DECLARATION()
+
+#endif // DATABASE_PLAYER_H
