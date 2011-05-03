95296b9de53eb641a43534afc2a8cce455155fa0
Niclas Raabe <screamer2002@gmx.de>
2011-05-03 18:26:55 +0200

    Still-not-working-changes
    Signed-off-by: Niclas Raabe <screamer2002@gmx.de>

--- Database/attributehash.cpp
+++ Database/attributehash.cpp
@@ -1,30 +1,3 @@
 #include "attributehash.h"
 
 using namespace Database;
-
-template<class Key, class V>
-AttributeHash<Key,V>::AttributeHash() :
-    QHash<Key,V>(parent),
-    AttributeHashBase()
-{
-}
-
-template<class Key, class V>
-Q_INLINE_TEMPLATE typename QHash<Key, V>::iterator AttributeHash<Key,V>::insert ( const Key & key, const V & value ){
-    QHash<Key,V>::insert(key,value);
-    emit AttributeHash<Key,V>::changed();
-}
-
-template<class Key, class V>
-QString AttributeHash<Key,V>::toString(){
-    QString string = "";
-    foreach(Key k, this->keys()){
-        string += k + ":" + this->value(k) + ", ";
-    }
-    return string;
-}
-
-
-AttributeHashBase::AttributeHashBase()
-{
-}
--- Database/attributehash.h
+++ Database/attributehash.h
@@ -23,11 +23,37 @@ class AttributeHash : public QHash<Key,V>, public AttributeHashBase
 public:
     explicit AttributeHash();
 
-    Q_INLINE_TEMPLATE typename QHash<Key, V>::iterator insert ( const Key & key, const V & value );
+    typename QHash<Key, V>::iterator insert ( const Key & key, const V & value );
 
     QString toString();
 
 };
+
+
+AttributeHashBase::AttributeHashBase()
+{
+}
+
+template<class Key, class V>
+AttributeHash<Key,V>::AttributeHash() :
+    QHash<Key,V>()
+{
+}
+
+template<class Key, class V>
+typename QHash<Key, V>::iterator AttributeHash<Key,V>::insert ( const Key & key, const V & value ){
+    QHash<Key,V>::insert(key,value);
+    emit AttributeHash<Key,V>::changed();
+}
+
+template<class Key, class V>
+QString AttributeHash<Key,V>::toString(){
+    QString string = "";
+    foreach(Key k, this->keys()){
+        string += k + ":" + this->value(k) + ", ";
+    }
+    return string;
+}
 
 }
 
