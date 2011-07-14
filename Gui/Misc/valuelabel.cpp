ab6a385bbd9ef9ec18e1377f978235d7afd5545d
Niklas Wulf <niklas@wulf-home.de>
2011-07-14 17:22:34 +0200

    Ein paar kleinere Sachen.

--- Gui/Misc/valuelabel.cpp
+++ Gui/Misc/valuelabel.cpp
@@ -24,7 +24,6 @@ void ValueLabel::paintEvent(QPaintEvent *event){
     QPainter* painter = new QPainter(this);
     QRect cr = this->contentsRect();
     cr.adjust(0,1,0,1);
-    painter->begin(this);
     painter->setPen(QColor("black"));
     painter->setBrush(QColor("black"));
     painter->setFont(font());
