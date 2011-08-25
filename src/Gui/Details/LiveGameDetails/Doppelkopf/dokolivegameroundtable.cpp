6aba94f90d9568a3c906487443e1a07cac2a4510
Niklas Wulf <niklas@wulf-home.de>
2011-08-25 21:34:35 +0200

    #23 resolved: Bei alten Daten stand (S) statt (R ) in der RoundTable.

--- src/Gui/Details/LiveGameDetails/Doppelkopf/dokolivegameroundtable.cpp
+++ src/Gui/Details/LiveGameDetails/Doppelkopf/dokolivegameroundtable.cpp
@@ -23,7 +23,7 @@ void DokoLiveGameRoundTable::addRound(::Database::Round* round)
         QTableWidgetItem* item = new QTableWidgetItem("");
         item->setFlags(item->flags() & ~Qt::ItemIsEditable & ~Qt::ItemIsSelectable);
         QString text = QString::number(dokoround->points->value(p));
-        if(dokoround->doko_soloPlayerId->value() != 0){
+        if(dokoround->doko_soloPlayerId->value() > 0){
             if (dokoround->doko_re->value(p)){
                 text += " (S";
                 if(dokoround->doko_hochzeitPlayerId->value() == p->id())
