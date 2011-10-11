89dd33e6b35ebbe66783acd9ea4916c9b20b925b
Niclas Raabe <niclasraabe@Niclass-MacBook-Pro.local>
2011-10-11 18:43:18 +0200

    Weitere Details im DokoLiveGameSummaryWidget

--- src/Gui/Details/LiveGameDetails/Doppelkopf/dokolivegamesummarywidget.cpp
+++ src/Gui/Details/LiveGameDetails/Doppelkopf/dokolivegamesummarywidget.cpp
@@ -62,6 +62,7 @@ void DokoLiveGameSummaryWidget::setupDokoStatsTab(){
     QWidget* statsWidget = new QWidget(this);
     QGridLayout* statsLayout = new QGridLayout(this);
 
+    // Re vs. Contra Stats
     QGridLayout* roundWinsLayout = new QGridLayout(this);
     roundWinsLayout->addWidget(new QLabel(tr("ReWins"),this),0,0);
     roundWinsLayout->addWidget(new QLabel(tr("ContraWins"),this),1,0);
@@ -73,7 +74,95 @@ void DokoLiveGameSummaryWidget::setupDokoStatsTab(){
     roundWinsLayout->addWidget(contraWins,1,1);
     statsLayout->addLayout(roundWinsLayout,0,0);
 
+    //Doko-Besonderheiten-Stats
+    QGridLayout* specialsLayout = new QGridLayout(this);
+    if(m_dokoLiveGame->doko_mitHochzeit->value()){
+        specialsLayout->addWidget(new QLabel(tr("Hochzeiten"),this),0,0);
+        QLabel* hochzeitCount = new QLabel(tr(""),this);
+        m_dokoLiveGame->doko_hochzeitCount->futureWatcher()->connectTo(hochzeitCount);
+        specialsLayout->addWidget(hochzeitCount,0,1);
+        QString players = "";
+        foreach(Database::Player* p, m_dokoLiveGame->players->value()){
+            if(m_dokoLiveGame->doko_hochzeit->value(p) > 0){
+                players += p->name->value() + " (" + QString::number(m_dokoLiveGame->doko_hochzeit->value(p)) + "), ";
+            }
+        }
+        players = players.remove(players.length()-2,players.length()-1);
+        specialsLayout->addWidget(new QLabel(players,this),1,1);
 
+    }
+    if(m_dokoLiveGame->doko_mitSolo->value()){
+        specialsLayout->addWidget(new QLabel(tr("Soli"),this),2,0);
+        QLabel* soloCount = new QLabel(tr(""),this);
+        m_dokoLiveGame->doko_soloCount->futureWatcher()->connectTo(soloCount);
+        specialsLayout->addWidget(soloCount,2,1);
+        QString players = "";
+        foreach(Database::Player* p, m_dokoLiveGame->players->value()){
+            if(m_dokoLiveGame->doko_solo->value(p) > 0){
+                players += p->name->value() + " (" + QString::number(m_dokoLiveGame->doko_solo->value(p)) + "), ";
+            }
+        }
+        players = players.remove(players.length()-2,players.length()-1);
+        specialsLayout->addWidget(new QLabel(players,this),3,1);
+    }
+    if(m_dokoLiveGame->doko_mitPflichtsolo->value()){
+        specialsLayout->addWidget(new QLabel(tr("Pflicht-Soli"),this),4,0);
+        QLabel* pflichtSoloCount = new QLabel(tr(""),this);
+        m_dokoLiveGame->doko_pflichtSoloCount->futureWatcher()->connectTo(pflichtSoloCount);
+        specialsLayout->addWidget(pflichtSoloCount,4,1);
+        QString players = "";
+        foreach(Database::Player* p, m_dokoLiveGame->players->value()){
+            if(m_dokoLiveGame->doko_hasPflichtSolo->value(p)){
+                players += p->name->value() + " (" + "1" + "), ";
+            }
+        }
+        players = players.remove(players.length()-2,players.length()-1);
+        specialsLayout->addWidget(new QLabel(players,this),5,1);
+    }
+    if(m_dokoLiveGame->doko_mitTrumpfabgabe->value()){
+        specialsLayout->addWidget(new QLabel(tr("Trumpfabgaben"),this),6,0);
+        QLabel* trumpfabgabeCount = new QLabel(tr(""),this);
+        m_dokoLiveGame->doko_trumpfabgabeCount->futureWatcher()->connectTo(trumpfabgabeCount);
+        specialsLayout->addWidget(trumpfabgabeCount,6,1);
+        QString players = "";
+        foreach(Database::Player* p, m_dokoLiveGame->players->value()){
+            if(m_dokoLiveGame->doko_trumpfabgabe->value(p) > 0){
+                players += p->name->value() + " (" + QString::number(m_dokoLiveGame->doko_trumpfabgabe->value(p)) + "), ";
+            }
+        }
+        players = players.remove(players.length()-2,players.length()-1);
+        specialsLayout->addWidget(new QLabel(players,this),7,1);
+
+    }
+    if(m_dokoLiveGame->doko_mitSchweinerei->value()){
+        specialsLayout->addWidget(new QLabel(tr("Schweinereien"),this),8,0);
+        QLabel* schweinereiCount = new QLabel(tr(""),this);
+        m_dokoLiveGame->doko_schweinereiCount->futureWatcher()->connectTo(schweinereiCount);
+        specialsLayout->addWidget(schweinereiCount,8,1);
+        QString players = "";
+        foreach(Database::Player* p, m_dokoLiveGame->players->value()){
+            if(m_dokoLiveGame->doko_schweinerei->value(p) > 0){
+                players += p->name->value() + " (" + QString::number(m_dokoLiveGame->doko_schweinerei->value(p)) + "), ";
+            }
+        }
+        players = players.remove(players.length()-2,players.length()-1);
+        specialsLayout->addWidget(new QLabel(players,this),9,1);
+     }
+    if(m_dokoLiveGame->doko_mitSchmeisserei->value()){
+        specialsLayout->addWidget(new QLabel(tr("Schmeissereien"),this),10,0);
+        QLabel* schmeissereiCount = new QLabel(tr(""),this);
+        m_dokoLiveGame->doko_schmeissereiCount->futureWatcher()->connectTo(schmeissereiCount);
+        specialsLayout->addWidget(schmeissereiCount,10,1);
+        QString players = "";
+        foreach(Database::Player* p, m_dokoLiveGame->players->value()){
+            if(m_dokoLiveGame->doko_schmeisserei->value(p) > 0){
+                players += p->name->value() + " (" + QString::number(m_dokoLiveGame->doko_schmeisserei->value(p)) + "), ";
+            }
+        }
+        players = players.remove(players.length()-2,players.length()-1);
+        specialsLayout->addWidget(new QLabel(players,this),11,1);
+    }
+    statsLayout->addLayout(specialsLayout,1,0);
 
     statsWidget->setLayout(statsLayout);
     ui->tabWidget->addTab(statsWidget,tr("DokoGameStats"));
