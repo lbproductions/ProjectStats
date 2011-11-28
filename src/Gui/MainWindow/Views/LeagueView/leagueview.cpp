26937a770895edb4de16890f8e0509f16b31e35b
Niklas Wulf <niklas@wulf-home.de>
2011-11-28 23:33:22 +0100

    TabBar vom LeagueView gestyled.

--- src/Gui/MainWindow/Views/LeagueView/leagueview.cpp
+++ src/Gui/MainWindow/Views/LeagueView/leagueview.cpp
@@ -28,10 +28,37 @@ LeagueView::LeagueView(Database::LeagueFolderCategorie *categorie, MainWindow *p
     m_statswidget = new LeagueViewDetails::LeagueStatsWidget(this);
 
     QTabWidget* tabwidget = new QTabWidget(this);
-    //tabwidget->setStyleSheet(" QTabBar::tab {background: rgb(51,51,51); min-width: 200px; color: white; padding: 2px; border: 1px solid white;}");
-    tabwidget->addTab(m_overviewwidget,"Overview");
-    tabwidget->addTab(m_graphwidget,"Graphs");
-    tabwidget->addTab(m_statswidget,"Stats");
+    tabwidget->setStyleSheet("QTabWidget::pane {" /* The tab widget frame */
+                                "border: none;"
+                                "background-image: url(:/graphics/styles/mac/tabbar/background);"
+                                "background-repeat: repeat-x;"
+                                "margin-top: -22px;"
+                                "padding-top: 22px;"
+                             "}"
+                             "QTabBar::tab {"
+                                "font-family: Lucida Grande;"
+                                "font-size: 10px;"
+                                "background: transparent;"
+                                "height: 16px;"
+                             "margin-top: 3px;"
+                             "margin-bottom: 3px;"
+                             "margin-right: 13px;"
+                             "}"
+                             "QTabBar::tab:selected {"
+                                "font-weight: bold;"
+                                "color: white;"
+                                "border: 8px transparent;"
+                                "border-left: 14px transparent;"
+                                "border-right: 14px transparent;"
+                                "border-image: url(:/graphics/styles/mac/tabbar/button) 8px 16px 16px 8px;"
+                                "padding-top: -8px;"
+                                "padding-bottom: -8px;"
+                                "padding-left: -6px;"
+                                "padding-right: -6px;"
+                             "}");
+    tabwidget->addTab(m_overviewwidget,tr("Overview"));
+    tabwidget->addTab(m_graphwidget,tr("  Graphs  "));
+    tabwidget->addTab(m_statswidget,tr(" Statistics "));
     layout()->addWidget(tabwidget);
 
 }
